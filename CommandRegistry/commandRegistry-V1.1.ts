34fde28051a884a692d9c239e5cb4e8d819be6b3
Nathan93705 <99233134+Nathan93705@users.noreply.github.com>
2024-03-14 19:03:05 -0700

    Add files via upload

---
+++ CommandRegistry/commandRegistry-V1.1.ts
@@ -0,0 +1,123 @@
+/* ***************************************
+
+            Command Registry
+               Version 1.1
+
+****************************************/
+
+
+import { Commands, Packet, PermissionLevel } from '@serenityjs/bedrock-protocol';
+import { Player, Serenity } from '@serenityjs/serenity';
+import { error } from 'console';
+
+interface CommandData {
+    name: string,
+    description: string,
+    permissionLevel: PermissionLevel,
+}
+interface RegistryCommand {
+    commandData: CommandData,
+    callBack: (player: Player, args: string[]) => void
+}
+
+let registeredCommands: RegistryCommand[] = []
+let registryStarted: boolean = false
+let instance: Serenity | undefined;
+
+/**
+* The Command Registry.
+*/
+export namespace CommandRegistry {
+
+    /**
+    * Starts The Command Registry.
+    * @param   serenity The serenity instance.
+    */
+    export function startRegistry(serenity: Serenity) {
+        if (registryStarted) return
+        registryStarted = true
+        instance = serenity
+        let network = serenity.network
+        network.before(Packet.CommandRequest, ({ packet, session }) => {
+            let commands = CommandRegistry.getCommands()
+
+            let parsedCommand = packet.rawCommand
+                .slice(1)
+                .trim()
+                .replace(/([~^][^~^\s]*)/g, "$1 ")
+                .match(/"[^"]+"|[^\s]+/g)
+                .map((e) => e.replace(/"(.+)"/, "$1").toString());
+
+            for (const RegistryCommand of commands) {
+                if (RegistryCommand.commandData.name === parsedCommand[0]) {
+                    RegistryCommand.callBack(session.player, parsedCommand.slice(1))
+                }
+            }
+            return true
+        })
+        network.before(Packet.AvailableCommands, ({ packet, session }) => {
+            let commands = CommandRegistry.getCommands()
+            for (const RegistryCommand of commands) {
+                let command: Commands = {
+                    name: RegistryCommand.commandData.name,
+                    description: RegistryCommand.commandData.description,
+                    flags: 0,
+                    permissionLevel: RegistryCommand.commandData.permissionLevel,
+                    alias: 0,
+                    subcommands: [],
+                    overloads: []
+                }
+                packet.commands.push(command)
+            }
+            return true
+        })
+    }
+    /**
+    * Registers A Command To The Registry.
+    * @param data The Command Data.
+    * @param callBack The Callback That Runs When The Registered Command Is Ran
+    * @example 
+    * new CommandRegistry().register(
+    * {permissionLevel: PermissionLevel.Member,name: "hello",description: "Basic Hello World Command"}, 
+    * (player, args) => {player.sendMessage(`Hello World`)}
+    * )
+    */
+    export function register(data: CommandData, callBack: (player: Player, args: string[]) => void) {
+        if (!registryStarted) {
+            throw error(`Error Registerying Command; Registry Is Not Started Run startRegistry() To Start It`)
+        }
+        if (data.description.length < 1) {
+            throw error(`Error Registerying Command; Command Description Must Be At Least 1 Character Long`)
+        }
+        if (data.name.length < 1) {
+            throw error(`Error Registerying Command; Command Name Must Be At Least 1 Character Long`)
+        }
+        if (!(data.permissionLevel in PermissionLevel)) {
+            throw error(`Error Registerying Command; Permission Level "${data.permissionLevel}" Doesnt Exist`)
+        }
+        if (this.commandExist(data.name)) {
+            registeredCommands.push({ commandData: data, callBack: callBack })
+            if (!instance) return
+            instance.logger.info(`[Command Registry] Registered "${data.name}"`)
+        } else {
+            throw error(`Error Registerying Command; Command "${data.name}" is already registered`)
+        }
+    }
+    /**
+    * Fetches The Command Registry List.
+    * @returns An Array Of The Registered Commands
+    */
+    export function getCommands(): RegistryCommand[] {
+        return registeredCommands
+    }
+    /**
+    * Checks The Command Registry If A Command Exists.
+    * @returns Boolean
+    */
+    export function commandExist(commandName: string): boolean {
+        return !registeredCommands.some(({ commandData }) => {
+            if (commandData.name === commandName) return true
+            else return false
+        })
+    }
+}
